06147f7fc48fd553662cde2a5a6dacb0a71db69f
Dilshan <dilshanwijerathna426@gmail.com>
2026-02-03 19:24:52 +0530

    making some changes prisma schema and make logics

--- app/feedback/page.tsx
+++ app/feedback/page.tsx
@@ -1,5 +1,28 @@
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+
 export default async function FeedbackPage() {
-        return (
+        
+  const {userId}=await auth();
+  
+  const posts=await prisma.post.findMany({
+    include:{
+      author:true,
+    votes:true,
+    },
+    orderBy:{
+      createdAt:'desc',
+    },
+  });
+
+  const categories=await prisma.post.groupBy({
+    by:['category'],
+    _count:{
+      id:true,
+    },
+  });
+  
+  return (
           <div>
             <h1>Feedback Dashboard</h1>
             <p>Welcome to the feedback dashboard. Here you can view and manage user feedback.</p>
